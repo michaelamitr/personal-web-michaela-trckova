8981ef8f4a0470dc470431248d5c06fd1df68383
Michaela Trčková <misa.trckova@volny.cz>
2023-06-15 18:15:25 +0200

    home page basic setup, edit needed

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './home.css';
+import michaelaImage from './img/IMG_3434.jpg';
 
 export const Home = () => {
   return (
-    <div>
-      <p></p>
-    </div>
+    <section id="home">
+      <div className="home__container">
+        <div className="description__container">
+          <h1 className="home__headline">Michaela Trčková</h1>
+          <h2 className="home__occupation">front-end developer</h2>
+        </div>
+        <img
+          src={michaelaImage}
+          alt="Michaela Trčková, profile picture"
+          className="michaela__image"
+        ></img>
+      </div>
+    </section>
   );
 };
