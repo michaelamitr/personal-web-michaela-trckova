a604b043fc87b1d87cada8bd58eecfc51bf1fee8
Michaela Trčková <misa.trckova@volny.cz>
2023-06-20 14:57:47 +0200

    skills page extension

--- src/pages/Skills/skills.jsx
+++ src/pages/Skills/skills.jsx
@@ -18,11 +18,21 @@ export const Skills = () => {
         <li className="skill-item">
           <div className="skill-heading">
             <div className="js-icon"></div>
-            <div className="react-icon"></div>Java Script, React
+            Java Script
           </div>
           <p className="skill-text">
             Communication with server, components, parent-child-child
-            communication, using API, routing.
+            communication, using API, routing, high-order functions, work with
+            arrays, cycles, own DOM elements.
+          </p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="react-icon"></div>React
+          </div>
+          <p className="skill-text">
+            Communication with server, components, props mechanism,
+            parent-child-child communication, using API, routing.
           </p>
         </li>
         <li className="skill-item">
@@ -30,13 +40,18 @@ export const Skills = () => {
             <div className="git-icon"></div>
             <div className="github-skill-icon"></div>Git, GitHub
           </div>
-          <p className="skill-text">Cooperation in a team.</p>
+          <p className="skill-text">
+            Cooperation in a team, work in branches, solving conflicts.
+          </p>
         </li>
         <li className="skill-item">
           <div className="skill-heading">
             <div className="figma-icon"></div>Figma
           </div>
-          <p className="skill-text">Constructing a prototype.</p>
+          <p className="skill-text">
+            Constructing a functional wireframe and prototype. UI and UX design
+            thinking and good eye for detail.
+          </p>
         </li>
       </ul>
     </section>
