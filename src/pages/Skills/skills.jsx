53f4c62baf398fe61277ce8fc77e614e2014b1b8
Michaela Trckova <trckova@zensys.cz>
2024-01-07 09:04:23 +0100

    skills update next js, ts, jira, ms, bootstrap, fm

--- src/pages/Skills/skills.jsx
+++ src/pages/Skills/skills.jsx
@@ -7,8 +7,25 @@ export const Skills = () => {
       <ul className="skills__container">
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="html-icon"></div>
-            <div className="css-icon"></div>HTML & CSS
+            <div className="icon ts-icon"></div>TypeScript
+          </div>
+          <p className="skill-text">
+            Using data types, creating own interfaces.
+          </p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="icon nextjs-icon"></div>Next.js
+          </div>
+          <p className="skill-text">
+            Able to build an efficient web and read the documentation to further
+            improve Next.js efficiency.
+          </p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="icon html-icon"></div>
+            <div className="icon css-icon"></div>HTML & CSS
           </div>
           <p className="skill-text">
             Use for coding good loking content and usage in combination with
@@ -17,7 +34,7 @@ export const Skills = () => {
         </li>
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="js-icon"></div>
+            <div className="icon js-icon"></div>
             JavaScript
           </div>
           <p className="skill-text">
@@ -28,7 +45,7 @@ export const Skills = () => {
         </li>
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="react-icon"></div>React
+            <div className="icon react-icon"></div>React
           </div>
           <p className="skill-text">
             Communication with server, components, props mechanism, data
@@ -39,8 +56,22 @@ export const Skills = () => {
         </li>
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="git-icon"></div>
-            <div className="github-skill-icon"></div>Git, GitHub
+            <div className="icon bootstrap-icon"></div>Bootstrap
+          </div>
+          <p className="skill-text">
+            Responsive styling, reading the documentation.
+          </p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="icon fm-icon"></div>Framer Motion
+          </div>
+          <p className="skill-text">Good looking animations.</p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="icon git-icon"></div>
+            <div className="icon github-skill-icon"></div>Git, GitHub
           </div>
           <p className="skill-text">
             Cooperation in a team, work in branches, solving conflicts.
@@ -48,7 +79,14 @@ export const Skills = () => {
         </li>
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="figma-icon"></div>Figma
+            <div className="icon ms-icon"></div>
+            <div className="icon jira-icon"></div>MS 365, Jira
+          </div>
+          <p className="skill-text">Team cooperation, work management.</p>
+        </li>
+        <li className="skill-item">
+          <div className="skill-heading">
+            <div className="icon figma-icon"></div>Figma
           </div>
           <p className="skill-text">
             Constructing a functional wireframe and prototype. UI and UX design
@@ -57,9 +95,9 @@ export const Skills = () => {
         </li>
         <li className="skill-item">
           <div className="skill-heading">
-            <div className="lr-icon"></div>
-            <div className="ps-icon"></div>
-            <div className="pr-icon"></div>
+            <div className="icon lr-icon"></div>
+            <div className="icon ps-icon"></div>
+            <div className="icon pr-icon"></div>
             Adobe Lightroom, Photoshop, Premiere Pro
           </div>
           <p className="skill-text">
